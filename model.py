06dbdbd40ca78d32808cce6790e53bdbad7627fd
Christo S. Christov <ch-hristov@users.noreply.github.com>
2022-12-27 10:31:06 -0500

    Add files via upload

---
+++ model.py
@@ -0,0 +1,125 @@
+from typing import List
+from PIL import Image
+
+class Line:
+    def __init__(self, line_id: str, start_xy: List[float], end_xy: List[float], tag: str, types: str):
+        self.line_id = line_id
+        self.start_xy = start_xy
+        self.types = types
+        self.tag = tag
+        self.start_xy = start_xy
+        self.end_xy = end_xy
+
+    def __repr__(self) -> str:
+        return str(self.line_id)
+
+class OtherLine:
+    def __init__(self, start_xy: List[float], end_xy: List[float], isOne: int):
+        self.start_xy = start_xy
+        self.end_xy = end_xy
+        self.isOne = isOne
+
+
+class Table:
+    def __init__(self):
+        self.lines = []
+
+class Word:
+    def __init__(self, word_id: str, start_xy: List[int], end_xy: List[int], text: str, value: int):
+        self.word_id = word_id
+        self.start_xy = start_xy
+        self.end_xy = end_xy
+        self.text = text
+        self.value = value
+
+    def __repr__(self) -> str:
+        return str(self.word_id)
+
+
+class Link:
+    def __init__(self, source: str, items: List[str]):
+        self.source = source
+        self.targets = items
+
+
+class PID:
+
+    def get_word_crop_by_index(self, i):
+        img = Image.open(self.path)
+        word = self.words[i]
+
+        x, y = word.start_xy
+        x1, y1 = word.end_xy
+
+        crop_target = min(x, x1), min(y, y1), max(x, x1) + 1, max(y, y1)+1
+
+        output = img.crop(crop_target)
+
+        return output
+
+    def get_symbol_crop_by_index(self, i):
+        img = Image.open(self.path)
+        symbol = self.symbols[i]
+
+        x, y = symbol.start_xy
+        x1, y1 = symbol.end_xy
+
+        crop_target = min(x, x1), min(y, y1), max(x, x1) + 1, max(y, y1)+1
+
+        output = img.crop(crop_target)
+
+        return output
+
+    def get_symbol_crop_by_id(self, idx):
+        img = Image.open(self.path)
+        symbol = [ops for ops in self.symbols if ops.symbol_id == idx]
+
+        x, y = symbol[0].start_xy
+        x1, y1 = symbol[0].end_xy
+
+        crop_target = min(x, x1), min(y, y1), max(x, x1) + 1, max(y, y1)+1
+        output = img.crop(crop_target)
+
+        return output
+
+    def get_line_crop_by_index(self, i):
+        img = Image.open(self.path)
+        line = self.lines[i]
+
+        x, y = line.start_xy
+        x1, y1 = line.end_xy
+
+        crop_target = (min(x, x1), min(y, y1), max(x, x1) + 1, max(y, y1)+1)
+
+        output = img.crop(crop_target)
+        return output
+
+    def __init__(self):
+        self.symbols = []
+        self.lines = []
+        self.words = []
+        self.table = []
+        self.links = []
+        self.details = []
+        self.otherLines = []
+        self.path = ''
+        self.id = ''
+        self.width = -1
+        self.height = -1
+
+    def __repr__(self) -> str:
+        return 'Symbols: {0}, Lines: {1}, Words: {2}, Table: {3}'.format(len(self.symbols),
+                                                                         len(self.lines),
+                                                                         len(self.words),
+                                                                         len(self.table))
+
+class Symbol:
+    def __init__(self, symbol_id: str, rect_start_xy: List[int], rect_end_xy: List[int], label: str, pid: PID):
+        self.symbol_id = symbol_id
+        self.start_xy = rect_start_xy
+        self.end_xy = rect_end_xy
+        self.label = label
+        self.pid = pid
+
+    def __repr__(self) -> str:
+        return str(self.symbol_id)
