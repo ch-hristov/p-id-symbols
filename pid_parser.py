90f18a384c74fe40b01c6a840429b3d12f07f9f2
Christo S. Christov <ch-hristov@users.noreply.github.com>
2023-01-11 09:59:07 -0500

    Update labels

--- pid_parser.py
+++ pid_parser.py
@@ -90,10 +90,10 @@ class PIDParser:
             top_y = bot_y
 
         if width < 1:
-            width = 1
+            width = 10
 
         if height < 1:
-            height = 1
+            height = 10
 
         return top_x / img_size[0], top_y / img_size[1], abs(width) / img_size[0], abs(height) / img_size[1]
 
